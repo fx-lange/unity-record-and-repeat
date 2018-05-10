fe9426234aa6aa11acd44d37fb5943a290f0a370
Felix Lange <felix.lange@ixds.com>
2018-05-10 15:56:00 +0200

    initial structure of recorder and recording

---
+++ Recorder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace TrackingRecorder
+{
+
+  public class Recorder : MonoBehaviour
+  { 
+
+    static string recordingsPath = "DataRecordings";
+		
+		private Recording recording = null;
+
+    void Start()
+    {
+      recording = ScriptableObject.CreateInstance<Recording>();
+      recording.recName = "UNIQUE NAME";
+      SaveRecording(recording);
+    }
+
+    void StartRecording()
+    {
+
+    }
+
+    void StopRecording()
+    {
+
+    }
+		
+		void RecordData(string data){
+			recording.addData(data);
+		}
+
+    static void SaveRecording(Recording recording)
+    {
+      string path = "Assets/" + recordingsPath;
+      if (!AssetDatabase.IsValidFolder(path))
+      {
+        AssetDatabase.CreateFolder("Assets", recordingsPath);
+      }
+
+      string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + recording.recName + ".asset");
+
+      AssetDatabase.CreateAsset(recording, assetPathAndName);
+
+      AssetDatabase.SaveAssets();
+      AssetDatabase.Refresh();
+    }
+  }
+
+}
+
---
+++ Recording.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace TrackingRecorder
+{
+  //TODO probably we would never create it via menu ...
+  [CreateAssetMenu(fileName = "OPRecording", menuName = "TrackingRecorder/Recording", order = 1)]
+  public class Recording : ScriptableObject
+  {
+
+    public class FrameData
+    {
+      public float time;
+      public string data;
+    }
+
+    private float startTimeSec;
+    private float duration;
+    private List<FrameData> dataFrames = new List<FrameData>();
+
+    public string recName = "My Recording";
+
+    Recording()
+    {
+      startTimeSec = Time.realtimeSinceStartup;
+      duration = 0;
+    }
+
+    public void addData(string data)
+    {
+      FrameData frameData = new FrameData();
+      frameData.time = Time.realtimeSinceStartup - startTimeSec;
+      frameData.data = data;
+      
+      duration = frameData.time; //always as long as the last data frame
+    }
+  }
+}
