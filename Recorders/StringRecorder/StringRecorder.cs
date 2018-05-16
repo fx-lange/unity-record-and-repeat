d8fced7c183bb668582f233e87fc44ec7681a834
Felix Lange <felix.lange@ixds.com>
2018-05-16 21:23:02 +0200

    rename folder

---
+++ Recorders/StringRecorder/StringRecorder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RecordForTimeline
+{
+    public class StringRecorder : Recorder
+    {
+        protected override Recording CreateInstance()
+        {
+            return ScriptableObject.CreateInstance<StringRecording>();
+        }
+
+        public void RecordData(string data)
+        {
+            StringData opData = new StringData();
+            opData.data = data;
+
+            RecordData(opData);
+        }
+    }
+}
