8f0884ecf1a02938e093ad4c26cd01a838ce5fc4
Felix Lange <felix.lange@ixds.com>
2018-05-18 07:13:23 +0200

    added a example showcasing recording and playback of mouse positions

---
+++ Examples/MouseDrawer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RecordAndPlay;
+
+public class MouseDrawer : DataListener
+{
+
+    private Vector3 mouseWorldPos;
+	
+	//TODO activate draw gizmos in game view!
+
+    public override void ProcessData(DataFrame results){
+		StringData json = results as StringData;
+		mouseWorldPos = JsonUtility.FromJson<Vector3>(json.data);
+		Debug.Log(json);
+	}
+
+    void OnDrawGizmos()
+    {
+		Gizmos.color = Color.green;
+        Gizmos.DrawSphere(mouseWorldPos, 1);
+    }
+}
---
+++ Examples/MouseRecorder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RecordAndPlay;
+
+//record mouse position as world coordinates
+public class MouseRecorder : StringRecorder
+{
+
+    private Vector3 mouseWorldPos;
+
+    protected new void Update()
+    {
+        base.Update();
+
+        Vector3 mouse = Input.mousePosition;
+        mouse.z = 10;
+
+        mouseWorldPos = Camera.main.ScreenToWorldPoint(mouse);
+
+        if (isRecording)
+        {
+            string json = JsonUtility.ToJson(mouseWorldPos);
+            Debug.Log(json);
+            RecordData(json);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (isRecording)
+        {
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.grey;
+        }
+        Gizmos.DrawSphere(mouseWorldPos, 1);
+    }
+}
