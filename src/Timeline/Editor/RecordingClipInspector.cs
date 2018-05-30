e01cfcde69ff38dcfb0d7956e2b4cd0a68a7989a
Felix Lange <felix.lange@ixds.com>
2018-05-30 21:50:34 +0200

    implement as clip editor instead of behaviour drawer

--- src/Timeline/Editor/RecordingClipInspector.cs
+++ src/Timeline/Editor/RecordingClipInspector.cs
@@ -27,38 +27,43 @@ using UnityEngine.Playables;
 
 using RecordAndPlay;
 
-[CustomPropertyDrawer(typeof(RecordingBehaviour))]
-public class RecordingBehaviourDrawer : PropertyDrawer
+[CustomEditor(typeof(RecordingClip), true)]
+public class RecordingClipInspector : Editor
 {
     public static bool disableRecordingSwitch = true;
-    
-    public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
-    {
-        int fieldCount = 4;
-        return fieldCount * EditorGUIUtility.singleLineHeight;
-    }
 
-    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
-    {
-        SerializedProperty recordingProp = property.FindPropertyRelative("recording");
+    // public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
+    // {
+    //     int fieldCount = 0;
+    //     return fieldCount * EditorGUIUtility.singleLineHeight;
+    // }
 
-        Rect singleFieldRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
-        
-        GUI.enabled = !(RecordingBehaviourDrawer.disableRecordingSwitch && Application.isPlaying);
-        EditorGUI.PropertyField(singleFieldRect, recordingProp);
+    public override void OnInspectorGUI()
+    {
+        SerializedProperty templateProp = serializedObject.FindProperty("template");
+        SerializedProperty recordingProp = templateProp.FindPropertyRelative("recording");
+
+        // Rect singleFieldRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+
+        GUI.enabled = !(RecordingClipInspector.disableRecordingSwitch && Application.isPlaying);
+        EditorGUILayout.PropertyField(recordingProp);
         GUI.enabled = true;
-        
+
         Recording recordingRef = recordingProp.objectReferenceValue as Recording;
         if (recordingRef)
         {
-            singleFieldRect.y += EditorGUIUtility.singleLineHeight;
-            EditorGUI.LabelField(singleFieldRect, "Recording Name", recordingRef.name);
-            
-            singleFieldRect.y += EditorGUIUtility.singleLineHeight;
-            EditorGUI.LabelField(singleFieldRect,"Duration", String.Format("{0:N2}",recordingRef.duration));
-            
-            singleFieldRect.y += EditorGUIUtility.singleLineHeight;
-            EditorGUI.LabelField(singleFieldRect, "Frame Count", recordingRef.FrameCount().ToString());
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+            // singleFieldRect.y += EditorGUIUtility.singleLineHeight;
+            EditorGUILayout.LabelField("Recording Name", recordingRef.name);
+
+            // singleFieldRect.y += EditorGUIUtility.singleLineHeight;
+            EditorGUILayout.LabelField("Duration", String.Format("{0:N2}", recordingRef.duration));
+
+            // singleFieldRect.y += EditorGUIUtility.singleLineHeight;
+            EditorGUILayout.LabelField("Frame Count", recordingRef.FrameCount().ToString());
+
+            EditorGUILayout.EndVertical();
         }
     }
 }
