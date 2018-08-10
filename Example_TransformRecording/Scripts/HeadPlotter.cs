61f10ef57b35e4425079127b325d0e05bd248f9e
Felix Lange <felix.lange@ixds.com>
2018-08-10 14:25:36 +0200

    new example started for tracking player/head movement

---
+++ Example_TransformRecording/Scripts/HeadPlotter.cs
@@ -0,0 +1,89 @@
+﻿// MIT License
+
+// Copyright (c) 2018 Felix Lange 
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RecordAndPlay;
+
+public class HeadPlotter : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class ColoredRecording
+    {
+        public Recording recording = null;
+        public Color color = Color.white;
+    }
+
+    public List<ColoredRecording> recordings = new List<ColoredRecording>();
+    
+    [Header("Size")]
+    public float radius = 0.5f;
+    public float rayLength = 0.1f;
+
+    [Range(0, 1)]
+    public float connectionAlpha = 1f;
+
+    void OnDrawGizmos()
+    {
+        
+        foreach(ColoredRecording coloredRec in recordings)
+        {
+            Recording recording = coloredRec.recording;
+            if(recording == null)
+            {
+                continue;
+            }
+
+            //draw colored recording
+            HeadRecorder.Head lastFrame = null;
+            foreach(DataFrame frame in recording.DataFrames)
+            {
+
+                //parse data
+                StringData stringData = frame as StringData;
+                HeadRecorder.Head head = JsonUtility.FromJson<HeadRecorder.Head>(stringData.data);
+
+                //draw head
+                Gizmos.color = coloredRec.color;
+                head.Draw(radius, rayLength);
+
+                //draw connection between heads
+                if (lastFrame != null)
+                {
+                    SetGizmoAlpha(connectionAlpha);
+                    Gizmos.DrawLine(lastFrame.worldPos, head.worldPos);
+                }
+                lastFrame = head;
+            }
+        }
+    }
+
+    void SetGizmoAlpha(float alpha)
+    {
+        Color drawColor = Gizmos.color;
+        drawColor.a = alpha;
+        Gizmos.color = drawColor;
+    }
+}
