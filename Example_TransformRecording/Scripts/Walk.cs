12edfcfb42b9089fc3423b0971cfb17edb6b6c27
Felix Lange <felix.lange@ixds.com>
2018-08-11 08:11:08 +0200

    simple walker script added

---
+++ Example_TransformRecording/Scripts/Walk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+	public float rotationSpeed = 5f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * moveSpeed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * moveSpeed;
+        }
+        
+		if (Input.GetKey("a"))
+        {
+			transform.Rotate(new Vector3(0,-rotationSpeed,0));
+        }
+        else if (Input.GetKey("d"))
+        {
+			transform.Rotate(new Vector3(0,+rotationSpeed,0));
+        }
+    }
+}
