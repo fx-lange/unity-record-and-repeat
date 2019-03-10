a5df34401369f975d1eecfcf171ca218ee331650
Felix Lange <langefx@gmail.com>
2019-03-10 22:53:20 +0100

    property drawer for data frames

---
+++ Source/Recording/Editor/DataFrameDrawer.cs
@@ -0,0 +1,48 @@
+﻿// MIT License
+
+// Copyright (c) 2018 Felix Lange 
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using UnityEditor;
+using UnityEngine;
+using RecordAndRepeat;
+
+[CustomPropertyDrawer(typeof(DataFrameBase),true)]
+public class DataFrameDrawer : PropertyDrawer
+{
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        var timeRect = new Rect(position.x + 90, position.y, position.width - 90, EditorGUIUtility.singleLineHeight);
+        float time = property.FindPropertyRelative("time").floatValue;
+        EditorGUI.LabelField(timeRect, $"t: {time.ToString("0.00")}s");
+        
+        
+        EditorGUI.PropertyField(position,property,true);
+
+        EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label){
+        return EditorGUI.GetPropertyHeight(property,label);
+    }
+}
